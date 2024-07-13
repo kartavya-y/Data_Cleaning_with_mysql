213875f77c88971429fcc2118932e523248fb9b6
kartavya-y <158798612+kartavya-y@users.noreply.github.com>
2024-07-14 00:00:35 +0530

    Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -23,8 +23,8 @@ SELECT * FROM world_layoffs.layoffs;
 -- the following steps were followed to clean the data
 -- 1. check for duplicates and remove any
 -- 2. standardize data and fix errors
--- 3. Look at null values and see what 
--- 4. remove any columns and rows that are not necessary - few ways
+-- 3. Looking at null values
+-- 4. remove any columns and rows that are not necessary
 
 
 
